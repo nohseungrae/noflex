6c72989d2f2737d0d15153f09b85a7ca79b95812
nohseungrae <ca10205@naver.com>
2019-11-15 00:37:04 +0900

    third

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,6 +9,7 @@ import {
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Detail from "../Routes/Detail";
 
 export default () => {
   return (
@@ -19,6 +20,8 @@ export default () => {
           <Route path="/" exact component={Home} />
           <Route path="/tv" component={TV} />
           <Route path="/search" component={Search} />
+          <Route path="/movie/:id" component={Detail} />
+          <Route path="/show/:id" component={Detail} />
           <Redirect from="*" to="/" />
         </Switch>
       </>
